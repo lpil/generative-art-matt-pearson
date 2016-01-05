31885c8b943e022fc688966e550be5e2b5db97be
Louis Pilfold <louis@lpil.uk>
2016-01-05 17:02:16 +0000

    Flag two!

--- src/art/flag_two.clj
+++ src/art/flag_two.clj
@@ -1,23 +1,29 @@
-(ns art.core
+(ns art.flag-two
   (:require [quil.core :as q]
             [quil.middleware]))
 
 (defn setup []
+  1
   )
 
-(defn update-state [_]
+(defn update-state [state]
+  (+ 1 state)
   )
 
-(defn draw [_]
+(defn draw [state]
   (q/background 0 101 189)
   (q/stroke-weight 30)
   (q/stroke 255)
-  (q/line 0 0 (q/width) (q/height))
-  (q/line (q/width) 0 0 (q/height))
+
+  (q/translate (/ (q/width) 2) (/ (q/height) 2))
+  (q/rotate (/ state 100))
+  (q/line -161 -100 161 100)
+  (q/line -161 100 161 -100)
+  (q/line 0 200 0 -200)
   )
 
 
-(q/defsketch example
+(q/defsketch flag-two
   :title "Art!"
   :middleware [quil.middleware/fun-mode]
   :settings #(q/smooth 2)
